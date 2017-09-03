d783b5af4349638d1c12991d5438fd7ac4f4d48d
Nawaf Abdullah <nawaf97k@gmail.com>
2017-09-03 16:51:29 -0400

    Add files via upload

---
+++ spaceobject.cpp
@@ -0,0 +1,65 @@
+#include "spaceobject.h"
+#include <iostream>
+using namespace std;
+
+SpaceObject::SpaceObject()
+{
+}
+
+//Setters
+////Object's Specifications
+void SpaceObject::SetParameters(char name, double vo, double elev, double bear, double m){
+    ObjName        = name;
+    Velocity       = vo;
+    ElevationAngle = elev;
+    BearingAngle   = bear;
+    Mass           = m;
+}
+void SpaceObject::SetDimensions(double l,double w, double h){
+    Length = l;
+    Width  = w;
+    Height = h;
+}
+////Object's Trajectory
+void SpaceObject::SetTimeInterval(int SimTime){
+    XTrajectoy = new double[SimTime];
+    YTrajectoy = new double[SimTime];
+    ZTrajectoy = new double[SimTime];
+    TimeLine   = new double[SimTime];
+}
+void SpaceObject::SetTrajectories(double Xp, double Yp, double Zp, int t){
+    XTrajectoy[t] = Xp;
+    YTrajectoy[t] = Yp;
+    ZTrajectoy[t] = Zp;
+}
+////Collision Detection
+void SpaceObject::SetComparisonPoint(int cp){ComparisonPoint = cp;}
+
+//Getters
+////Object's Specifications
+char   SpaceObject::GetName(){return ObjName;}
+double SpaceObject::GetVelocity(){return Velocity;}
+double SpaceObject::GetElevationAngle(){return ElevationAngle;}
+double SpaceObject::GetBearingAngle(){return BearingAngle;}
+double SpaceObject::GetMass(){return Mass;}
+double SpaceObject::GetWidth(){return Width;}
+double SpaceObject::GetHeight(){return Height;}
+double SpaceObject::GetLength(){return Length;}
+////Object's Trajectory
+double SpaceObject::GetXPoints(int  tp){return XTrajectoy[tp];}
+double SpaceObject::GetYPoints(int  tp){return YTrajectoy[tp];}
+double SpaceObject::GetZPoints(int  tp){return ZTrajectoy[tp];}
+double SpaceObject::GetTimeLine(int tp){return TimeLine[tp];}
+////Collision Detection Operator Overload
+bool   SpaceObject::operator==(const SpaceObject& SpObj){
+    if(this->XTrajectoy[ComparisonPoint]-0.50*Length <= SpObj.XTrajectoy[ComparisonPoint]+0.50*Length &&
+       this->YTrajectoy[ComparisonPoint]-0.50*Width  <= SpObj.YTrajectoy[ComparisonPoint]+0.50*Width  &&
+       this->ZTrajectoy[ComparisonPoint]-0.50*Height <= SpObj.ZTrajectoy[ComparisonPoint]+0.50*Height)
+        return true;
+    else if (this->XTrajectoy[ComparisonPoint]+0.50*Length >= SpObj.XTrajectoy[ComparisonPoint]-0.50*Length &&
+             this->YTrajectoy[ComparisonPoint]+0.50*Width  >= SpObj.YTrajectoy[ComparisonPoint]-0.50*Width  &&
+             this->ZTrajectoy[ComparisonPoint]+0.50*Height >= SpObj.ZTrajectoy[ComparisonPoint]-0.50*Height)
+        return true;
+    else
+        return false;
+}
---
+++ spaceobject.h
@@ -0,0 +1,54 @@
+#ifndef SPACEOBJECT_H
+#define SPACEOBJECT_H
+
+
+class SpaceObject
+{
+public:
+    SpaceObject();
+    //Setters
+    ////Object's Specifications
+    void SetParameters(char, double, double, double, double);
+    void SetDimensions(double, double, double);
+    ////Object's Trajectory
+    void SetTimeInterval(int);
+    void SetTrajectories(double, double, double, int);
+    ////Collision Detection
+    void SetComparisonPoint(int);
+
+    //Getters
+    ////Object's Specifications
+    char   GetName();
+    double GetVelocity();
+    double GetElevationAngle();
+    double GetBearingAngle();
+    double GetMass();
+    double GetWidth();
+    double GetHeight();
+    double GetLength();
+    ////Object's Trajectory
+    double GetXPoints(int);
+    double GetYPoints(int);
+    double GetZPoints(int);
+    double GetTimeLine(int);
+    ////Collision Detection
+    bool operator==(const SpaceObject&);
+    ~SpaceObject();
+
+protected:
+    //Specifications related variables
+    char   ObjName;
+    double Velocity;
+    double BearingAngle, ElevationAngle;
+    double Mass;
+    double Length = 1, Width = 1, Height = 1;
+    //Trajectory arrays
+    double *XTrajectoy;
+    double *YTrajectoy;
+    double *ZTrajectoy;
+    double *TimeLine;
+    //Collision Detection
+    int ComparisonPoint;
+};
+
+#endif // SPACEOBJECT_H
