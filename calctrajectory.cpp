649ae27aba740741fd620c08c2a3162d1f28b992
Nawaf Abdullah <nawaf97k@gmail.com>
2017-09-04 16:06:57 -0400

    Update calctrajectory.cpp
    New actually working trajectory calculation functions

--- calctrajectory.cpp
+++ calctrajectory.cpp
@@ -3,13 +3,15 @@
 #include <math.h>
 using namespace std;
 
+
 calcTrajectory::calcTrajectory(){}
 
 //Setters
-void calcTrajectory::SetStartLocation(double xo, double yo, double zo){
-    xi = xo;
-    yi = yo;
-    zi = zo;
+void calcTrajectory::SetTimeInterval(int SimTime){
+    ArraySize = SimTime;
+    Xcoords = new double[ArraySize];
+    Ycoords = new double[ArraySize];
+    Zcoords = new double[ArraySize];
 }
 void calcTrajectory::EnableGravity(char grav){
     if (grav == 'y' || grav == 'Y')
@@ -23,27 +25,46 @@ void calcTrajectory::EnableGravity(int grav){
     else
         g = 0;
 }
+void calcTrajectory::SetStartLocation(double xo, double yo, double zo){
+    xi = xo;
+    yi = yo;
+    zi = zo;
+}
 void calcTrajectory::TakeObjectParameters(double vel, double ele, double bea, double mass){
     vo   = vel;
     elev = ele;
     bear = bea;
     m    = mass;
 }
-void calcTrajectory::SetTimeInterval(int SimTime){
-    Xcoords = new double[SimTime];
-    Ycoords = new double[SimTime];
-    Zcoords = new double[SimTime];
+void calcTrajectory::NoForceTrajectory(){
+    for(int i = 0; i <= ArraySize; i++){
+        Xcoords[i] = vo*cos(bear*0.0174533)*cos(elev*0.0174533)*i + xi;
+        Ycoords[i] = vo*sin(bear*0.0174533)*cos(elev*0.0174533)*i + yi;
+        Zcoords[i] = vo*sin(elev*0.0174533)*i - 0.5*g*pow(i,2)   + zi;
+    }
 }
+void calcTrajectory::ApplyForce(double fx, double fy, double fz, int ti, int tf){
+    ts = ti;
+    for(ti; ti <= tf; ti++){
+        dt = ts - ti;
+        Xcoords[ti] = Xcoords[ti] + 0.5*(fx/m)*pow(dt,2);
+        Ycoords[ti] = Ycoords[ti] + 0.5*(fy/m)*pow(dt,2);
+        Zcoords[ti] = Zcoords[ti] + 0.5*(fz/m)*pow(dt,2);
+    }
+}
+
+/*
 //Problems here
 ///Forces dt is not correctly defined
 ///Didn't account for no force intervals
 void calcTrajectory::ApplyForce(double fx, double fy, double fz, int ti, int tf){
-    for (ti; ti < tf; ti++) {
+    for (ti; ti <= tf; ti++) {
         Xcoords[ti] = vo*cos(bear*0.0174533)*cos(elev*0.0174533)*ti + 0.5*(fx/m)*pow(ti,2) + xi;
         Ycoords[ti] = vo*sin(bear*0.0174533)*cos(elev*0.0174533)*ti + 0.5*(fy/m)*pow(ti,2) + yi;
         Zcoords[ti] = vo*sin(elev*0.0174533)*ti - 0.5*g*pow(ti,2)   + 0.5*(fz/m)*pow(ti,2) + zi;
     }
 }
+*/
 //void calcTrajectory::ResetCalculator(){t = 0;}
 void calcTrajectory::CleanUpWhendone(){
     delete[] Xcoords;
